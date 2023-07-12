a9a2fe0aedd4fcb51eee64086c416451fa5529ed
Md. Nuruzaman Milon <njmilon1@gmail.com>
2023-07-12 11:21:15 +0600

    remove local state management & error from user dashboard

--- app/user-dashboard/my-order/page.jsx
+++ app/user-dashboard/my-order/page.jsx
@@ -5,9 +5,9 @@ import React from 'react'
 const MyOrders = () => {
   return (
     <div className='container my-10 flex flex-col gap-y-4'>
-        <h2 className='text-xl font-semibold'>Hello, Sadit Shekh</h2>
-        <DashboardUtil />
-        <MyOrder />
+      <h2 className='text-xl font-semibold'>Hello, Sadit Shekh</h2>
+      <DashboardUtil />
+      <MyOrder />
     </div>
   )
 }
