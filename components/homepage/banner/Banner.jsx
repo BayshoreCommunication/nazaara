0ac1d1e5871650d1596b419ce81b82cba06c17e4
AbdullahIsha <shahariaresha@gmail.com>
2023-08-28 19:19:13 +0600

    frontend bug fix 2

--- components/homepage/banner/Banner.jsx
+++ components/homepage/banner/Banner.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import React from "react";
 
 const Banner = async () => {
-  const url = `http://localhost:8000/api/v1/customization/64d9fb77f3a7ce9915b44b6f`;
+  const url = `${process.env.API_URL}/api/v1/customization/64d9fb77f3a7ce9915b44b6f`;
   const allData = await FetchServerSideData(url);
   const data = allData?.data?.bannerSection;
   return (
