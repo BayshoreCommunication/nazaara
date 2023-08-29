cd09e9719591c2436435d86ad5a140637c54b504
AbdullahIsha <shahariaresha@gmail.com>
2023-08-29 16:00:12 +0600

    category filter

--- components/navbar/DesktopNavbar.jsx
+++ components/navbar/DesktopNavbar.jsx
@@ -1,60 +1,60 @@
-'use client'
-import Image from 'next/image'
-import Link from 'next/link'
-import Cart from '../shopping-cart/Cart'
-import SignIn from '../authentication/SignIn'
-import SignUp from '../authentication/SignUp'
-import { useCallback, useEffect, useState } from 'react'
-import { getCookie } from 'cookies-next'
-import axios from 'axios'
-import Loader from '../Loader'
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import Cart from "../shopping-cart/Cart";
+import SignIn from "../authentication/SignIn";
+import SignUp from "../authentication/SignUp";
+import { useCallback, useEffect, useState } from "react";
+import { getCookie } from "cookies-next";
+import axios from "axios";
+import Loader from "../Loader";
 
 const DesktopNavbar = () => {
-  const apiUrl = `${process.env.API_URL}/api/v1/product/categories`
-  const [categories, setCategories] = useState(null)
-  const [auth, setAuth] = useState('signIn')
-  const [isAuth, setIsAuth] = useState(false)
-  const [isCartOpen, setIsCartOpen] = useState(false)
-  const [imgUrl, setImgUrl] = useState(null)
+  const apiUrl = `${process.env.API_URL}/api/v1/product/categories`;
+  const [categories, setCategories] = useState(null);
+  const [auth, setAuth] = useState("signIn");
+  const [isAuth, setIsAuth] = useState(false);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [imgUrl, setImgUrl] = useState(null);
 
-  const jsonStr = getCookie('userAuthCredential')
+  const jsonStr = getCookie("userAuthCredential");
 
   const getCookieData = useCallback(() => {
     if (jsonStr != null) {
-      const obj = JSON.parse(jsonStr)
-      setImgUrl(obj.imageUrl)
+      const obj = JSON.parse(jsonStr);
+      setImgUrl(obj.imageUrl);
     }
-  }, [jsonStr])
+  }, [jsonStr]);
 
   const fetchData = useCallback(async () => {
     try {
-      const response = await axios.get(apiUrl)
+      const response = await axios.get(apiUrl);
       if (response.status === 200) {
-        setCategories(response.data.newData)
+        setCategories(response.data.newData);
       }
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }, [apiUrl])
+  }, [apiUrl]);
 
   useEffect(() => {
-    getCookieData()
-    fetchData()
-  }, [getCookieData, jsonStr, apiUrl, fetchData])
+    getCookieData();
+    fetchData();
+  }, [getCookieData, jsonStr, apiUrl, fetchData]);
 
   const handleAuth = () => {
-    setIsCartOpen(false)
-    setIsAuth(!isAuth)
-  }
+    setIsCartOpen(false);
+    setIsAuth(!isAuth);
+  };
 
   const handleCartOpen = () => {
-    setIsAuth(false)
-    setIsCartOpen(!isCartOpen)
-  }
+    setIsAuth(false);
+    setIsCartOpen(!isCartOpen);
+  };
 
-  if (!categories) {
-    return <Loader height="h-[15vh]" />
-  }
+  // if (!categories) {
+  //   return <Loader height="h-[15vh]" />;
+  // }
 
   return (
     <div className="hidden lg:block container py-4">
@@ -101,10 +101,10 @@ const DesktopNavbar = () => {
               {/* User Authentication Content */}
               {isAuth && (
                 <>
-                  {auth === 'signIn' && (
+                  {auth === "signIn" && (
                     <SignIn setAuth={setAuth} setIsAuth={setIsAuth} />
                   )}
-                  {auth === 'signUp' && (
+                  {auth === "signUp" && (
                     <SignUp setAuth={setAuth} setIsAuth={setIsAuth} />
                   )}
                 </>
@@ -130,119 +130,77 @@ const DesktopNavbar = () => {
 
       <div className="flex justify-between items-center mt-2">
         <div>
-          <ul className="flex gap-2 2xl:gap-4">
-            <div className="group">
-              <li className="font-medium relative bg-base-100 text-primary-color cursor-pointer text-sm px-2 py-1 rounded-lg hover:underline underline-offset-4">
-                NEW ARRIVALS
-                <div className="h-6 w-full absolute lg:bottom-[-23px] xl:bottom-[-21px] left-0"></div>
-              </li>
-              <div className="hidden z-20 text-text-color group-hover:block bg-base-100 absolute w-full left-0 top-[160px] shadow-xl">
-                <div className="flex justify-between w-2/3 mx-auto py-6">
-                  <ul className="flex flex-col gap-y-2">
-                    <li className="text-primary-color font-semibold">SALE</li>
-                    <li>NEW ARRIVALS</li>
-                    <li>READY TO SHIP</li>
-                    <li>LIMITED STOCK</li>
-                    <li>DISCOUNT</li>
-                  </ul>
-                  <ul className="flex flex-col gap-y-2">
-                    <li className="text-primary-color font-semibold">
-                      SHOP BY CATEGORY
-                    </li>
-                    {categories.map((elem, index) => {
-                      if (index < 5) {
-                        return (
-                          <li key={index}>
-                            <Link
-                              href={`/products/categories/${elem.category}`}
-                            >
-                              {elem.category}
-                            </Link>
-                          </li>
-                        )
-                      }
-                    })}
-                  </ul>
-                  <Image
-                    src={categories[0].url}
-                    alt="logo"
-                    width={180}
-                    height={64}
-                    className="rounded-md border-2 border-[#d4af37]"
-                  />
-                  <Image
-                    src={categories[6].url}
-                    alt="logo"
-                    width={180}
-                    height={64}
-                    className="rounded-md border-2 border-[#d4af37]"
-                  />
-                </div>
+          {categories && (
+            <ul className="flex gap-2 2xl:gap-4">
+              <div>
+                <li className="font-medium relative bg-base-100 text-primary-color cursor-pointer text-sm px-2 py-1 rounded-lg hover:underline underline-offset-4">
+                  <Link href="/products">ALL PRODUCTS</Link>
+                </li>
               </div>
-            </div>
-            {categories.map((elem, index) => {
-              if (index < 3) {
-                return (
-                  <div className="group" key={index}>
-                    <li className="font-medium relative cursor-pointer text-sm px-2 py-1 rounded-lg hover:underline underline-offset-4">
-                      {elem.category}
-                      <div className="h-6 w-full absolute lg:bottom-[-23px] xl:bottom-[-21px] left-0"></div>
-                    </li>
-                    <div className="hidden text-text-color group-hover:block bg-base-100 absolute w-full left-0 top-[160px] z-20 shadow-xl">
-                      <div className="flex justify-between w-2/3 mx-auto py-6">
-                        <ul className="flex flex-col gap-y-2">
-                          <li className="text-primary-color font-semibold">
-                            SALE
-                          </li>
-                          <li>NEW ARRIVALS</li>
-                          <li>READY TO SHIP</li>
-                          <li>LIMITED STOCK</li>
-                          <li>DISCOUNT</li>
-                        </ul>
-                        <ul className="flex flex-col gap-y-2">
-                          <li className="text-primary-color font-semibold">
-                            SHOP BY CATEGORY
-                          </li>
-                          {categories.map((elem, index) => {
-                            if (index < 5) {
-                              return (
-                                <li key={index}>
-                                  <Link
-                                    href={`/products/categories/${elem.category}`}
-                                  >
-                                    {elem.category}
-                                  </Link>
-                                </li>
-                              )
-                            }
-                          })}
-                        </ul>
-                        <Image
-                          src={categories[3].url}
-                          alt="logo"
-                          width={180}
-                          height={64}
-                          className="rounded-md border-2 border-[#d4af37]"
-                        />
-                        <Image
-                          src={categories[5].url}
-                          alt="logo"
-                          width={180}
-                          height={64}
-                          className="rounded-md border-2 border-[#d4af37]"
-                        />
+              {categories.map((elem, index) => {
+                if (index < 3) {
+                  return (
+                    <div className="group" key={index}>
+                      <li className="font-medium relative cursor-pointer text-sm px-2 py-1 rounded-lg hover:underline underline-offset-4">
+                        {elem.category}
+                        <div className="h-6 w-full absolute lg:bottom-[-23px] xl:bottom-[-21px] left-0"></div>
+                      </li>
+                      <div className="hidden text-text-color group-hover:block bg-base-100 absolute w-full left-0 top-[160px] z-20 shadow-xl">
+                        <div className="flex justify-between w-2/3 mx-auto py-6">
+                          <ul className="flex flex-col gap-y-2">
+                            <li className="text-primary-color font-semibold">
+                              SALE
+                            </li>
+                            <li>NEW ARRIVALS</li>
+                            <li>READY TO SHIP</li>
+                            <li>LIMITED STOCK</li>
+                            <li>DISCOUNT</li>
+                          </ul>
+                          <ul className="flex flex-col gap-y-2">
+                            <li className="text-primary-color font-semibold">
+                              SHOP BY CATEGORY
+                            </li>
+                            {categories.map((elem, index) => {
+                              if (index < 5) {
+                                return (
+                                  <li key={index}>
+                                    <Link
+                                      href={`/products/categories/${elem.category}`}
+                                    >
+                                      {elem.category}
+                                    </Link>
+                                  </li>
+                                );
+                              }
+                            })}
+                          </ul>
+                          <Image
+                            src={categories[3].url}
+                            alt="logo"
+                            width={180}
+                            height={64}
+                            className="rounded-md border-2 border-[#d4af37]"
+                          />
+                          <Image
+                            src={categories[5].url}
+                            alt="logo"
+                            width={180}
+                            height={64}
+                            className="rounded-md border-2 border-[#d4af37]"
+                          />
+                        </div>
                       </div>
                     </div>
-                  </div>
-                )
-              }
-            })}
-            <div>
-              <li className="font-medium cursor-pointer text-sm px-2 py-1 rounded-lg hover:underline underline-offset-4">
-                <Link href="/location">OUR LOCATIONS</Link>
-              </li>
-            </div>
-          </ul>
+                  );
+                }
+              })}
+              <div>
+                <li className="font-medium cursor-pointer text-sm px-2 py-1 rounded-lg hover:underline underline-offset-4">
+                  <Link href="/location">OUR LOCATIONS</Link>
+                </li>
+              </div>
+            </ul>
+          )}
         </div>
         <form className="">
           <div className="relative">
@@ -274,7 +232,7 @@ const DesktopNavbar = () => {
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DesktopNavbar
+export default DesktopNavbar;
