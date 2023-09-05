65a086a780296f453afee566781e135efb622edc
AbdullahIsha <shahariaresha@gmail.com>
2023-09-05 18:52:27 +0600

    upadte

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import DesktopNavbar from "./DesktopNavbar";
 import MobileNavbar from "./MobileNavbar";
+import { useDispatch } from "react-redux";
+import { getCookie } from "cookies-next";
+import { addItemToCart } from "@/services/cartSlice";
 
 const Navbar = () => {
+  // add cart items to redux store from database
+  const dispatch = useDispatch();
+  const fetchData = async () => {
+    const jsonStr = getCookie("userAuthCredential");
+    try {
+      if (jsonStr) {
+        const obj = JSON.parse(jsonStr);
+        const response = await fetch(
+          `${process.env.API_URL}/api/v1/cart/user/${obj._id}`
+        );
+        const data = await response.json();
+
+        if (data) {
+          data.data.map((elem) => dispatch(addItemToCart(elem)));
+        }
+      }
+    } catch (error) {
+      console.error("Error fetching countries:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="bg-primary-color text-base-100 relative">
       {/* pc and desktop view  */}
