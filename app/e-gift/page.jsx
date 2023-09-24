706b66ea730a9225e5837fedbd5e0105f1696320
nuruzaman-milon <njmilon1@gmail.com>
2023-09-24 12:23:57 +0600

    e gift page created

--- app/e-gift/page.jsx
+++ app/e-gift/page.jsx
@@ -3,24 +3,42 @@ import { FaGift } from "react-icons/fa";
 
 const EGift = () => {
   return (
-    <div>
-      <h1 className="flex items-center justify-center gap-x-2 h-28 border-b-2 text-2xl font-medium text-gray-600">
+    <div className="container">
+      <h1 className="flex items-center justify-center gap-x-2 h-28 border-b-2 text-2xl font-medium text-gray-800">
         <FaGift />
         EGift Cards
       </h1>
-      <div className="container my-10 flex flex-col items-center gap-y-8">
-        <h1 className="text-2xl font-medium text-gray-600">JOIN OUR TEAM</h1>
-        <div className="text-center">
-          <p className="text-md font-medium text-gray-500">
-            Send Your CV Today At <br /> anzaralifestyleltd@gmail.com
+      <div className="my-10 flex flex-col lg:flex-row items-center gap-y-10">
+        <div className="flex-1">
+          <Image
+            src="/images/e-gift-cart.png"
+            width={1000}
+            height={500}
+            alt="Nazaara E-gift Cart"
+            className="w-[34rem]"
+          />
+          <Image
+            src="/images/e-gift-voucher.png"
+            width={1000}
+            height={500}
+            alt="Nazaara E-gift Voucher"
+            className="w-[34rem]"
+          />
+        </div>
+        <div className="flex flex-1 flex-col gap-y-6 text-center">
+          <h2 className="uppercase text-lg">
+            LAST MINUTE GIFT EASIER THAN EVER
+          </h2>
+          <p className="uppercase text-sm font-medium">
+            virtual gift cards make giving a last minute gift easier than ever.
+            Just buy an eGift card on nazaara and send it via email.
+          </p>
+          <p className="uppercase text-lg">HOW TO ORDER</p>
+          <p className="uppercase text-sm">
+            to buy a Egift card contact us on our facebook page or you can also
+            send a text on whatsapp.
           </p>
         </div>
-        <Image
-          src="/images/career-nazara.png"
-          width={613}
-          height={278}
-          alt="career nazaara"
-        />
       </div>
     </div>
   );
