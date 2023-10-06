021383e5e8e57fd0b5b9a501b7f54893de6192de
AbdullahEsha <shahariaresha@gmail.com>
2023-10-07 02:37:54 +0600

    update individual product page

--- app/user-dashboard/page.jsx
+++ app/user-dashboard/page.jsx
@@ -1,34 +1,34 @@
-"use client";
-import { deleteCookie, getCookie } from "cookies-next";
-import { useRouter } from "next/navigation";
-import Loader from "@/components/Loader";
-import DashboardUtil from "@/components/user-dashboard/DashboardUtil";
-import EditUserProfile from "@/components/user-dashboard/EditUserProfileModal";
+'use client'
+import { deleteCookie, getCookie } from 'cookies-next'
+import { useRouter } from 'next/navigation'
+import Loader from '@/components/Loader'
+import DashboardUtil from '@/components/user-dashboard/DashboardUtil'
+import EditUserProfile from '@/components/user-dashboard/EditUserProfileModal'
 // import usefetch from "@/customhooks/usefetch";
-import { useGetUserByIDQuery } from "@/services/userApi";
-import Image from "next/image";
-import Link from "next/link";
-import React, { useState } from "react";
+import { useGetUserByIDQuery } from '@/services/userApi'
+import Image from 'next/image'
+import Link from 'next/link'
+import React, { useState } from 'react'
 
 const UserDashboard = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
   //get authenticate user id using cookie
-  let getUserIdFromCookie;
-  const cookie = getCookie("userAuthCredential");
+  let getUserIdFromCookie
+  const cookie = getCookie('userAuthCredential')
   if (cookie != null) {
-    const obj = JSON.parse(cookie);
-    getUserIdFromCookie = obj._id;
+    const obj = JSON.parse(cookie)
+    getUserIdFromCookie = obj._id
   }
 
   //fetch specific user data using rtk query
-  const { data, isLoading } = useGetUserByIDQuery(getUserIdFromCookie);
+  const { data, isLoading } = useGetUserByIDQuery(getUserIdFromCookie)
 
   // when isLoading is false show loading spinner
   if (isLoading) {
-    return <Loader height="h-[90vh]" />;
+    return <Loader height="h-[90vh]" />
   }
-  const authUserData = data?.data;
+  const authUserData = data?.data
 
   // const date = new Date(authUserData?.createdAt);
   // const day = date.getDate().toString().padStart(2, "0");
@@ -36,28 +36,28 @@ const UserDashboard = () => {
   // const year = date.getFullYear();
   // const formattedDate = `${day}-${month}-${year}`;
 
-  const date = new Date(authUserData?.createdAt);
-  const day = date.getDate().toString().padStart(2, "0");
+  const date = new Date(authUserData?.createdAt)
+  const day = date.getDate().toString().padStart(2, '0')
 
   // Array of month names
   const monthNames = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ]
 
-  const monthName = monthNames[date.getMonth()]; // Get the month name
-  const year = date.getFullYear();
-  const formattedDate = `${day}, ${monthName} - ${year}`;
+  const monthName = monthNames[date.getMonth()] // Get the month name
+  const year = date.getFullYear()
+  const formattedDate = `${day}, ${monthName} - ${year}`
 
   return (
     <>
@@ -69,202 +69,84 @@ const UserDashboard = () => {
             Hello, {authUserData?.fullName}
           </h2>
           <DashboardUtil />
-          <div>
-            <div className="text-gray-600 flex flex-col lg:flex-row gap-4 items-start">
-              <div className="border rounded-lg py-3 px-4 flex-1">
-                <div className="flex gap-2 mb-2">
-                  <p className="text-lg font-medium text-gray-700">
-                    Personal Profile
-                  </p>
-                  <p> | </p>
-                  <button
-                    onClick={() => setIsModalOpen(true)}
-                    className="text-primary-color"
-                  >
-                    Edit
-                  </button>
-                </div>
-                <div className="flex flex-col gap-y-1 text-sm">
-                  <p>
-                    <span className="font-semibold">Name: </span>
-                    {authUserData?.fullName}
-                  </p>
-                  <p>
-                    <span className="font-semibold">Email: </span>{" "}
-                    {authUserData?.email}
-                  </p>
-                  {authUserData?.phone && (
-                    <p>
-                      <span className="font-semibold">Phone: </span>
-                      {authUserData?.phone}
-                    </p>
-                  )}
-                  {authUserData?.gender && (
-                    <p>
-                      <span className="font-semibold">Gender: </span>
-                      {authUserData?.gender}
-                    </p>
-                  )}
-
-                  <p>
-                    <span className="font-semibold"> Refund: </span>
-                    {authUserData?.refund}
-                  </p>
-                  <p>
-                    <span className="font-semibold">Joined Science: </span>
-                    {formattedDate}
-                  </p>
-                </div>
+          <div className="text-gray-600 flex flex-col lg:flex-row gap-4 items-start">
+            <div className="border rounded-lg py-3 px-4 flex-1">
+              <div className="flex gap-2 mb-2">
+                <p className="text-lg font-medium text-gray-700">
+                  Personal Profile
+                </p>
+                <p> | </p>
+                <button
+                  onClick={() => setIsModalOpen(true)}
+                  className="text-primary-color"
+                >
+                  Edit
+                </button>
               </div>
-              <div className="border rounded-lg py-3 px-4 flex-[2]">
-                <div className="flex gap-2 mb-2 items-center">
-                  <p className="text-lg font-medium text-gray-700">
-                    Address Book
+              <div className="flex flex-col gap-y-1 text-sm">
+                <p>
+                  <span className="font-semibold">Name: </span>
+                  {authUserData?.fullName}
+                </p>
+                <p>
+                  <span className="font-semibold">Email: </span>{' '}
+                  {authUserData?.email}
+                </p>
+                {authUserData?.phone && (
+                  <p>
+                    <span className="font-semibold">Phone: </span>
+                    {authUserData?.phone}
                   </p>
-                  <p> | </p>
-                  <Link href={`/user-dashboard/${authUserData?._id}`}>
-                    <button className="text-primary-color">More</button>
-                  </Link>
-                </div>
-
-                {authUserData?.addressBook && (
-                  <div className="flex gap-3">
-                    {authUserData?.addressBook.map((data) => (
-                      <div
-                        key={data._id}
-                        className="border-e-2 flex-1 text-sm flex flex-col gap-y-2"
-                      >
-                        <p className="text-md text-green-800 bg-slate-200 px-2 py-1 w-max text-xs rounded-sm bg-gray-200">
-                          {data?.addressType}
-                        </p>
-                        <p className=" font-medium text-gray-700">
-                          {data?.fullName}
-                        </p>
-                        <p>{`${data?.street}, ${data?.zip}`}</p>
-                        <p>{`${data?.city}, ${data?.country}`}</p>
-                        <p>{data?.phone}</p>
-                      </div>
-                    ))}
-                  </div>
                 )}
+                {authUserData?.gender && (
+                  <p>
+                    <span className="font-semibold">Gender: </span>
+                    {authUserData?.gender}
+                  </p>
+                )}
+                <p>
+                  <span className="font-semibold"> Refund: </span>
+                  {authUserData?.refund}
+                </p>
+                <p>
+                  <span className="font-semibold">Joined Science: </span>
+                  {formattedDate}
+                </p>
               </div>
             </div>
-            {/* recent order  */}
-            <div>
-              <div className="relative overflow-x-auto border py-2 px-4 rounded-lg text-gray-500 mt-4">
-                <h2 className="text-lg font-medium ml-2 mb-2">Recent Orders</h2>
-                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                  <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                      <th scope="col" className="px-6 py-3">
-                        Order
-                      </th>
-                      <th scope="col" className="px-6 py-3">
-                        Placed On
-                      </th>
-                      <th scope="col" className="px-6 py-3">
-                        Item
-                      </th>
-                      <th scope="col" className="px-6 py-3">
-                        Total
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                      <th
-                        scope="row"
-                        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                      >
-                        22322862836283
-                      </th>
-                      <td className="px-6 py-4">27/12/2022</td>
-                      <td className="px-6 py-4">
-                        <Image
-                          src="/images/category/bridal_top.png"
-                          alt="My Image"
-                          width={46}
-                          height={40}
-                        />
-                      </td>
-                      <td className="px-6 py-4">$2999</td>
-                    </tr>
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                      <th
-                        scope="row"
-                        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                      >
-                        22322862836283
-                      </th>
-                      <td className="px-6 py-4">27/12/2022</td>
-                      <td className="px-6 py-4">
-                        <Image
-                          src="/images/category/bridal_top.png"
-                          alt="My Image"
-                          width={46}
-                          height={40}
-                        />
-                      </td>
-                      <td className="px-6 py-4">$2999</td>
-                    </tr>
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                      <th
-                        scope="row"
-                        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                      >
-                        22322862836283
-                      </th>
-                      <td className="px-6 py-4">27/12/2022</td>
-                      <td className="px-6 py-4">
-                        <Image
-                          src="/images/category/bridal_top.png"
-                          alt="My Image"
-                          width={46}
-                          height={40}
-                        />
-                      </td>
-                      <td className="px-6 py-4">$2999</td>
-                    </tr>
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                      <th
-                        scope="row"
-                        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                      >
-                        22322862836283
-                      </th>
-                      <td className="px-6 py-4">27/12/2022</td>
-                      <td className="px-6 py-4">
-                        <Image
-                          src="/images/category/bridal_top.png"
-                          alt="My Image"
-                          width={46}
-                          height={40}
-                        />
-                      </td>
-                      <td className="px-6 py-4">$2999</td>
-                    </tr>
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                      <th
-                        scope="row"
-                        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                      >
-                        22322862836283
-                      </th>
-                      <td className="px-6 py-4">27/12/2022</td>
-                      <td className="px-6 py-4">
-                        <Image
-                          src="/images/category/bridal_top.png"
-                          alt="My Image"
-                          width={46}
-                          height={40}
-                        />
-                      </td>
-                      <td className="px-6 py-4">$2999</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
+          </div>
+          <div className="border rounded-lg py-3 px-4 flex-[2]">
+            <div className="flex gap-2 mb-2 items-center">
+              <p className="text-lg font-medium text-gray-700">Address Book</p>
+              <p> | </p>
+              <Link href={`/user-dashboard/${authUserData?._id}`}>
+                <button className="text-primary-color">More</button>
+              </Link>
             </div>
+
+            {authUserData?.addressBook && (
+              <div className="flex gap-3">
+                {authUserData?.addressBook.map((data, index) => (
+                  <div
+                    key={index}
+                    className={`flex-1 text-sm flex flex-col gap-y-2 ${
+                      index < authUserData?.addressBook.length - 1 &&
+                      'border-e-2'
+                    }`}
+                  >
+                    <p className="text-md text-green-800 bg-slate-200 px-2 py-1 w-max text-xs rounded-sm bg-gray-200">
+                      {data?.addressType}
+                    </p>
+                    <p className=" font-medium text-gray-700">
+                      {data?.fullName}
+                    </p>
+                    <p>{`${data?.street}, ${data?.zip}`}</p>
+                    <p>{`${data?.city}, ${data?.country}`}</p>
+                    <p>{data?.phone}</p>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
           {isModalOpen && (
             <EditUserProfile
@@ -275,7 +157,7 @@ const UserDashboard = () => {
         </div>
       )}
     </>
-  );
-};
+  )
+}
 
-export default UserDashboard;
+export default UserDashboard
--- components/product-detail/DetailImagePage.jsx
+++ components/product-detail/DetailImagePage.jsx
@@ -1,66 +1,36 @@
-import Image from "next/image";
-import { useSelector } from "react-redux";
+import Image from 'next/image'
+import { useSelector } from 'react-redux'
 
-import ImageGallery from "react-image-gallery";
+import ImageGallery from 'react-image-gallery'
 // import stylesheet if you're not already using CSS @import
-import "react-image-gallery/styles/css/image-gallery.css";
-
-const images = [
-  {
-    original: "https://picsum.photos/id/1018/1000/600/",
-    thumbnail: "https://picsum.photos/id/1018/250/150/",
-  },
-  {
-    original: "https://picsum.photos/id/1015/1000/600/",
-    thumbnail: "https://picsum.photos/id/1015/250/150/",
-  },
-  {
-    original: "https://picsum.photos/id/1019/1000/600/",
-    thumbnail: "https://picsum.photos/id/1019/250/150/",
-  },
-];
+import 'react-image-gallery/styles/css/image-gallery.css'
+import useWindowDimensions from '@/customhooks/useDimensionWindow'
 
 const DetailImage = ({ productData }) => {
-  // console.log(
-  //   "details",
-  //   productData.variant.map((data) => data.imageUrl.map((url) => url))
-  // );
-  // console.log("details", productData.variant[0].imageUrl[0]);
+  const { width } = useWindowDimensions()
+  const currentColor = useSelector((state) => state.imgFilter.color)
 
-  const currentColor = useSelector((state) => state.imgFilter.color);
-
-  let currentProduct;
+  let currentProduct
   if (currentColor)
     currentProduct = productData.variant.filter(
-      (el) => el.color === currentColor
-    );
-  else currentProduct = productData.variant;
+      (el) => el.color === currentColor,
+    )
+  else currentProduct = productData.variant
 
   const images = currentProduct?.flatMap((elem) =>
     elem.imageUrl.map((url) => ({
       original: url,
       thumbnail: url,
-    }))
-  );
-
-  // console.log("images", images);
+    })),
+  )
   return (
     <>
-      <ImageGallery thumbnailPosition={"left"} items={images} />
-
-      <>
-        {/* <div className="hidden lg:grid grid-cols-2 gap-4">
-          {currentProduct.map((elem) =>
-            elem.imageUrl.map((url, index) => (
-              <button key={index}>
-                <Image key={url} src={url} alt={url} width={600} height={800} />
-              </button>
-            ))
-          )}
-        </div> */}
-      </>
+      <ImageGallery
+        thumbnailPosition={`${width < 992 ? 'bottom' : 'left'}`}
+        items={images}
+      />
     </>
-  );
-};
+  )
+}
 
-export default DetailImage;
+export default DetailImage
