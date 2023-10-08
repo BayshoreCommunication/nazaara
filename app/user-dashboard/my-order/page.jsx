f32a694326ba8cf8504e7ecc38189f3e8d2c7a68
nasim67reja <nasimreja67@gmail.com>
2023-10-08 14:55:08 +0600

    my order page done

--- app/user-dashboard/my-order/page.jsx
+++ app/user-dashboard/my-order/page.jsx
@@ -1,15 +1,47 @@
-import DashboardUtil from '@/components/user-dashboard/DashboardUtil'
-import MyOrder from '@/components/user-dashboard/MyOrder'
-import React from 'react'
+"use client";
+
+import DashboardUtil from "@/components/user-dashboard/DashboardUtil";
+
+import MyOrder from "@/components/user-dashboard/MyOrder";
+import React, { useCallback, useEffect, useState } from "react";
+import { getCookie } from "cookies-next";
 
 const MyOrders = () => {
-  return (
-    <div className='container my-10 flex flex-col gap-y-4'>
-      <h2 className='text-xl font-semibold'>Hello, Sadit Shekh</h2>
-      <DashboardUtil />
-      <MyOrder />
-    </div>
-  )
-}
+  const [userData, setUserData] = useState();
+  const fetchData = useCallback(async () => {
+    const jsonStr = getCookie("userAuthCredential");
+    try {
+      if (jsonStr) {
+        const obj = JSON.parse(jsonStr);
+        const response = await fetch(
+          `${process.env.API_URL}/api/v1/user/${obj._id}`
+        );
 
-export default MyOrders
+        const data = await response.json();
+        setUserData(data.data);
+      }
+    } catch (error) {
+      console.error("Error fetching countries:", error);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  console.log("user data", userData);
+
+  return (
+    <>
+      {userData && (
+        <div className="container my-10 flex flex-col gap-y-4">
+          <h2 className="text-xl font-semibold">Hello, {userData.fullName}</h2>
+          <DashboardUtil />
+          <MyOrder userData={userData} />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default MyOrders;
--- components/user-dashboard/MyOrder.jsx
+++ components/user-dashboard/MyOrder.jsx
@@ -1,77 +1,120 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-const MyOrder = () => {
-  return (
-    <div className='flex flex-col gap-y-6 text-gray-600'>
-        <div className='rounded-lg border'>
-            <div className='py-1 border-b'>
-                <div className='px-4'>
-                    <p>Order <span className='text-sky-500'>#3423438438473</span></p>
-                    <p>Place on <span>4 jun 2023</span></p>
-                </div>
-            </div>
-            <div className='flex justify-around items-center my-4 w-full lg:w-4/5'>
-                <div className='flex gap-6 items-center'>
-                    <Image
-                    src="/images/category/bridal_top.png"
-                    alt="My Image"
-                    width={70}
-                    height={60}
-                    className='rounded-lg'
-                    />
-                    <p>Panache Apparel - BLUEBELL</p>
-                </div>
-                <p>QTY: 1</p>
-                <button className='bg-gray-300 rounded-full py-1 text-sm px-2'>Cancelled</button>
-            </div>
-        </div>
-        <div className='rounded-lg border'>
-            <div className='py-1 border-b'>
-                <div className='px-4'>
-                    <p>Order <span className='text-sky-500'>#3423438438473</span></p>
-                    <p>Place on <span>4 jun 2023</span></p>
-                </div>
-            </div>
-            <div className='flex justify-around items-center my-4 w-full lg:w-4/5'>
-                <div className='flex gap-6 items-center'>
-                    <Image
-                    src="/images/category/bridal_top.png"
-                    alt="My Image"
-                    width={70}
-                    height={60}
-                    className='rounded-lg'
-                    />
-                    <p>Panache Apparel - BLUEBELL</p>
-                </div>
-                <p>QTY: 1</p>
-                <button className='bg-gray-300 rounded-full py-1 text-sm px-2'>Cancelled</button>
-            </div>
-        </div>
-        <div className='rounded-lg border'>
-            <div className='py-1 border-b'>
-                <div className='px-4'>
-                    <p>Order <span className='text-sky-500'>#3423438438473</span></p>
-                    <p>Place on <span>4 jun 2023</span></p>
-                </div>
-            </div>
-            <div className='flex justify-around items-center my-4 w-full lg:w-4/5'>
-                <div className='flex gap-6 items-center'>
-                    <Image
-                    src="/images/category/bridal_top.png"
-                    alt="My Image"
-                    width={70}
-                    height={60}
-                    className='rounded-lg'
-                    />
-                    <p>Panache Apparel - BLUEBELL</p>
-                </div>
-                <p>QTY: 1</p>
-                <button className='bg-gray-300 rounded-full py-1 text-sm px-2'>Cancelled</button>
-            </div>
-        </div>
-    </div>
-  )
+function formatDate(inputDateString) {
+  const date = new Date(inputDateString);
+
+  // Define an array of month names
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  // Extract the day, month, and year from the date
+  const day = date.getDate();
+  const month = monthNames[date.getMonth()];
+  const year = date.getFullYear();
+
+  // Create the formatted date string
+  const formattedDateString = `${day} ${month} ${year}`;
+
+  return formattedDateString;
 }
 
-export default MyOrder
+const MyOrder = ({ userData }) => {
+  return (
+    <div className="flex flex-col gap-y-6 text-gray-600">
+      {userData.orders.length > 0 &&
+        userData.orders.map((el, i) => (
+          <div key={i} className="rounded-lg border">
+            <div className="py-1 border-b">
+              <div className="px-4">
+                <p>
+                  Order <span className="text-sky-500">{el._id}</span>
+                </p>
+                <p>
+                  Place on <span>{formatDate(el.createdAt)}</span>
+                </p>
+              </div>
+            </div>
+            <div className="flex justify-around items-center my-4 w-full lg:w-4/5">
+              <div className="flex gap-6 items-center">
+                <Image
+                  //   src="/images/category/bridal_top.png"
+                  src={el.product.variant[0].imageUrl[0]}
+                  alt="My Image"
+                  width={70}
+                  height={60}
+                  className="rounded-lg"
+                />
+                <p>
+                  {el.product.productName} - {el.product.sku}
+                </p>
+              </div>
+              <p>QTY: {el.quantity}</p>
+              <button className="bg-gray-300 rounded-full py-1 text-sm px-2">
+                {el.deliveryStatus}
+              </button>
+            </div>
+          </div>
+        ))}
+
+      {/* <div className='rounded-lg border'>
+            <div className='py-1 border-b'>
+                <div className='px-4'>
+                    <p>Order <span className='text-sky-500'>#3423438438473</span></p>
+                    <p>Place on <span>4 jun 2023</span></p>
+                </div>
+            </div>
+            <div className='flex justify-around items-center my-4 w-full lg:w-4/5'>
+                <div className='flex gap-6 items-center'>
+                    <Image
+                    src="/images/category/bridal_top.png"
+                    alt="My Image"
+                    width={70}
+                    height={60}
+                    className='rounded-lg'
+                    />
+                    <p>Panache Apparel - BLUEBELL</p>
+                </div>
+                <p>QTY: 1</p>
+                <button className='bg-gray-300 rounded-full py-1 text-sm px-2'>Cancelled</button>
+            </div>
+        </div>
+        <div className='rounded-lg border'>
+            <div className='py-1 border-b'>
+                <div className='px-4'>
+                    <p>Order <span className='text-sky-500'>#3423438438473</span></p>
+                    <p>Place on <span>4 jun 2023</span></p>
+                </div>
+            </div>
+            <div className='flex justify-around items-center my-4 w-full lg:w-4/5'>
+                <div className='flex gap-6 items-center'>
+                    <Image
+                    src="/images/category/bridal_top.png"
+                    alt="My Image"
+                    width={70}
+                    height={60}
+                    className='rounded-lg'
+                    />
+                    <p>Panache Apparel - BLUEBELL</p>
+                </div>
+                <p>QTY: 1</p>
+                <button className='bg-gray-300 rounded-full py-1 text-sm px-2'>Cancelled</button>
+            </div>
+        </div> */}
+    </div>
+  );
+};
+
+export default MyOrder;
