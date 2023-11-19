d83657a3259d66210b3c9d85e5acd1e0505ce5f8
nuruzaman-milon <njmilon1@gmail.com>
2023-11-19 14:28:34 +0600

    revalidate to 60s added

--- components/navbar/MainNavbar.jsx
+++ components/navbar/MainNavbar.jsx
@@ -2,20 +2,22 @@ import React from "react";
 import { cookies } from "next/headers";
 import NavBarContent from "./NavBarContent";
 
-// async function getData() {
-//   const cookieData = cookies();
-//   const userData = cookieData.get("userAuthCredential");
-//   if (userData) {
-//     const data = JSON.parse(userData?.value);
-//     const res = await fetch(
-//       `${process.env.API_URL}/api/v1/cart/user/${data._id}`
-//     );
-//     if (!res.ok) {
-//       throw new Error("Failed to fetch data");
-//     }
-//     return res.json();
-//   }
-// }
+export const revalidate = 60;
+
+async function getData() {
+  const cookieData = cookies();
+  const userData = cookieData.get("userAuthCredential");
+  if (userData) {
+    const data = JSON.parse(userData?.value);
+    const res = await fetch(
+      `${process.env.API_URL}/api/v1/cart/user/${data._id}`
+    );
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    return res.json();
+  }
+}
 
 async function getSalesData() {
   const res = await fetch(`${process.env.API_URL}/api/v1/category/nav-data`);
@@ -26,12 +28,12 @@ async function getSalesData() {
 }
 
 const MainNavbar = async () => {
-  // const data = await getData();
+  const data = await getData();
   const sales = await getSalesData();
 
   return (
     <main className="sticky top-0 z-50">
-      <NavBarContent sales={sales} />
+      <NavBarContent data={data} sales={sales} />
     </main>
   );
 };
