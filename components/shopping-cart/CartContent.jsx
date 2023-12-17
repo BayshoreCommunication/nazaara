7e54eb5085df96d8be1bd9f964cbe3a3bca555d9
nuruzaman-milon <njmilon1@gmail.com>
2023-12-17 18:44:45 +0600

    start working on coupon

--- components/shopping-cart/CartContent.jsx
+++ components/shopping-cart/CartContent.jsx
@@ -12,11 +12,13 @@ import ButtonOnHover from "../ButtonOnHover";
 import { FaHandPointLeft, FaTimes } from "react-icons/fa";
 import NoProductFound from "../NoProductFound";
 import Link from "next/link";
+import { IoMdArrowDropdown } from "react-icons/io";
 
 const CartContent = ({ userData }) => {
   const [updateCartLoading, setUpdateCartLoading] = useState([]);
   const [isDeleteLoading, setIsDeleteLoading] = useState(false);
   const [couponAmount, setCouponAmount] = useState(0);
+  const [couponCode, setCouponCode] = useState("");
 
   const { data: cartData, isLoading } = useGetCartByUserIdQuery(
     `${userData._id}`
@@ -118,20 +120,26 @@ const CartContent = ({ userData }) => {
   } else {
     const data = cartData?.data;
 
+    //calculate subtotal
     let subTotal = 0;
     data.map((data) => {
       subTotal = subTotal + data.product.salePrice * data.quantity;
     });
 
+    //calculate vat
     const vatIncluded = (subTotal * 0.07).toFixed(2);
 
+    //calculate total amount
     let totalAmount = subTotal - couponAmount;
 
     // console.log("amount", subTotal, vatIncluded, totalAmount);
 
     console.log("data", data);
 
-    // const is
+    const handleCoupon = () => {
+      console.log("Coupon Code:", couponCode);
+      //here using this couponCode value I need to check and manipulate total amount
+    };
     return (
       <div className="main-container flex gap-10 items-start">
         <div className="flex flex-[3] flex-col gap-5 bg-white p-4">
@@ -201,75 +209,97 @@ const CartContent = ({ userData }) => {
                             ৳ {detail?.product?.salePrice * detail?.quantity}
                             /-
                           </p>
-                          <div className="flex items-center">
-                            {detail?.quantity > 1 ? (
+                          <div className="relative group">
+                            <div>
+                              <span className="absolute opacity-0 z-50 group-hover:opacity-100 bottom-full px-0 py-1 text-xs w-full text-center font-medium text-gray-500 bg-gray-100 transition-opacity duration-300">
+                                {`${
+                                  detail.product.stock > 0
+                                    ? `${detail?.product?.stock} Stock Available`
+                                    : `${
+                                        detail.product.preOrder
+                                          ? "Pre-Order Available"
+                                          : "Out Of Stock"
+                                      }`
+                                }`}
+                              </span>
+                            </div>
+                            <div className="flex items-center">
+                              {detail?.quantity > 1 ? (
+                                <button
+                                  onClick={() =>
+                                    handleDecreaseQuantity(
+                                      detail?.variantId,
+                                      detail?.quantity,
+                                      detail?.user,
+                                      index
+                                    )
+                                  }
+                                  className={`flex items-center justify-center text-gray-600 border border-gray-400  hover:bg-gray-300 hover:text-gray-700 font-bold w-7 h-7 text-xl`}
+                                >
+                                  -
+                                </button>
+                              ) : (
+                                <button
+                                  onClick={() =>
+                                    handleDeleteCartItem(
+                                      detail?.variantId,
+                                      detail?.user
+                                    )
+                                  }
+                                  className={`flex items-center justify-center text-gray-600 border border-gray-400  hover:bg-gray-300 hover:text-gray-700 font-bold w-7 h-7 text-xl`}
+                                >
+                                  <MdDeleteForever />
+                                </button>
+                              )}
+
+                              {updateCartLoading[index] ? (
+                                <p className="text-gray-600 border border-gray-400 font-normal w-7 h-7 flex justify-center items-center">
+                                  <BeatLoader color="#820000" size={3} />
+                                </p>
+                              ) : (
+                                <p className="text-gray-600 border border-gray-400 font-normal w-7 h-7 flex justify-center items-center">
+                                  {detail?.quantity}
+                                </p>
+                              )}
                               <button
                                 onClick={() =>
-                                  handleDecreaseQuantity(
+                                  handleIncreaseQuantity(
                                     detail?.variantId,
                                     detail?.quantity,
                                     detail?.user,
+                                    detail?.product?.stock,
+                                    detail?.product?.preOrder,
                                     index
                                   )
                                 }
-                                className={`flex items-center justify-center text-gray-600 border border-gray-400  hover:bg-gray-300 hover:text-gray-700 font-bold w-7 h-7 text-xl`}
-                              >
-                                -
-                              </button>
-                            ) : (
-                              <button
-                                onClick={() =>
-                                  handleDeleteCartItem(
-                                    detail?.variantId,
-                                    detail?.user
-                                  )
+                                className={`flex items-center justify-center text-gray-600 border border-gray-400  hover:bg-gray-300 hover:text-gray-700 font-bold w-7 h-7 text-xl ${
+                                  detail?.quantity >= detail?.product?.stock
+                                    ? "cursor-not-allowed"
+                                    : "cursor-pointer"
+                                }`}
+                                disabled={
+                                  detail?.quantity >= detail?.product?.stock
                                 }
-                                className={`flex items-center justify-center text-gray-600 border border-gray-400  hover:bg-gray-300 hover:text-gray-700 font-bold w-7 h-7 text-xl`}
                               >
-                                <MdDeleteForever />
+                                +
                               </button>
-                            )}
-
-                            {updateCartLoading[index] ? (
-                              <p className="text-gray-600 border border-gray-400 font-normal w-7 h-7 flex justify-center items-center">
-                                <BeatLoader color="#820000" size={3} />
-                              </p>
-                            ) : (
-                              <p className="text-gray-600 border border-gray-400 font-normal w-7 h-7 flex justify-center items-center">
-                                {detail?.quantity}
-                              </p>
-                            )}
-
-                            <button
-                              onClick={() =>
-                                handleIncreaseQuantity(
-                                  detail?.variantId,
-                                  detail?.quantity,
-                                  detail?.user,
-                                  detail?.product?.stock,
-                                  detail?.product?.preOrder,
-                                  index
-                                )
-                              }
-                              className={`flex items-center justify-center text-gray-600 border border-gray-400  hover:bg-gray-300 hover:text-gray-700 font-bold w-7 h-7 text-xl ${
-                                detail?.quantity >= detail?.product?.stock
-                                  ? "cursor-not-allowed"
-                                  : "cursor-pointer"
-                              }`}
-                              disabled={
-                                detail?.quantity >= detail?.product?.stock
-                              }
-                            >
-                              +
-                            </button>
+                            </div>
                           </div>
                         </div>
                         <Link
                           href={`/shop/cart/measurement?cartId=${detail?._id}&userId=${userData?._id}`}
+                          className="relative group"
                         >
+                          {detail.sizeChart && (
+                            <div>
+                              <span className="absolute opacity-0 z-50 group-hover:opacity-100 bottom-full px-2 py-1 text-xs w-full text-center font-medium text-gray-500 bg-gray-100 transition-opacity duration-300">
+                                Already Added
+                              </span>
+                            </div>
+                          )}
                           <button
                             disabled={detail.sizeChart}
-                            className=" bg-primary-color text-white px-2 py-1 text-xs rounded-sm hover:bg-primary-hover-color transition-colors duration-500"
+                            className="bg-primary-color text-white px-2 py-1 text-xs rounded-sm hover:bg-primary-hover-color transition-colors duration-500 disabled:cursor-not-allowed"
                           >
                             Add Measurement
                           </button>
@@ -303,9 +333,15 @@ const CartContent = ({ userData }) => {
             <input
               type="text"
               placeholder="Enter Coupon Code"
+              name="coupon"
+              value={couponCode}
+              onChange={(e) => setCouponCode(e.target.value)}
               className="focus:outline-none border border-gray-400 p-2 text-gray-600 text-sm rounded-md"
             />
-            <button className=" bg-primary-color text-white px-4 py-2 text-sm rounded-md hover:bg-primary-hover-color transition-colors duration-500">
+            <button
+              onClick={handleCoupon}
+              className=" bg-primary-color text-white px-4 py-2 text-sm rounded-md hover:bg-primary-hover-color transition-colors duration-500"
+            >
               APPLY
             </button>
           </div>
