2d41dd921b4b3cddcd61dd5e4a66e3313bbed8c0
nuruzaman-milon <njmilon1@gmail.com>
2023-12-28 18:26:31 +0600

    productCart component change

--- components/ProductCart.jsx
+++ components/ProductCart.jsx
@@ -7,13 +7,32 @@ import ReadyToShipBadge from "./ReadyToShipBadge";
 import { CalculatePercentage } from "@/helpers/CalculateDiscountPercentage";
 import { useInView } from "react-intersection-observer";
 import { MotionDiv } from "./MotionDiv";
+import { FcShipped } from "react-icons/fc";
+import { MdOutlineLocalShipping } from "react-icons/md";
+// import useSWR from "swr";
 
 const ProductCart = ({ data, i }) => {
+  // const [promotion, setPromotion] = useState({});
   const { ref, inView } = useInView();
   const variants = {
     hidden: { opacity: 0 },
     inView: { opacity: 1 },
   };
+  // const url = `${
+  //   process.env.API_URL
+  // }/api/v1/promotion/${data?.promotion?.toString()}`;
+  // console.log("product data", typeof data?.promotion?.toString());
+
+  // const fetcher = async () => {
+  //   const response = await fetch(url);
+  //   const data = await response.json();
+  //   return data;
+  // };
+
+  // const { data: couponData, error, isLoading } = useSWR("coupon", fetcher);
+
+  console.log("datatata", data);
+
   return (
     <>
       {data && (
@@ -50,6 +69,13 @@ const ProductCart = ({ data, i }) => {
                   />
                 )}
               </div>
+              {/* {data?.promotion && data?.promotion?.validPromotion && (
+                <div className="absolute top-2 right-2">
+                  <span className="text-black">
+                    {data?.promotion?.freeShipping && "free shipping available"}
+                  </span>
+                </div>
+              )} */}
             </div>
           </Link>
           <div className="h-20 text-left my-3 mx-2">
@@ -63,12 +89,24 @@ const ProductCart = ({ data, i }) => {
                   {data?.regularPrice}/-
                 </p>
               )}
+
+              {data?.promotion && data?.promotion?.validPromotion && (
+                <p className="text-white text-xs bg-green-500 px-2 py-[1px] rounded-md">
+                  {data?.promotion?.freeShipping && (
+                    <span className="flex items-center gap-1">
+                      <MdOutlineLocalShipping size={18} />
+                      Free Shipping
+                    </span>
+                  )}
+                </p>
+              )}
             </div>
             <p className="text-xs font-semibold my-2 text-gray-500">
               {data?.productName.length > 25
                 ? `${data.productName.slice(0, 25)}...`
                 : data?.productName}
             </p>
+
             {data?.preOrder && (
               <div className="">
                 <ReadyToShipBadge text="Pre-Order Available" />
--- components/card/BestSelling/MainDataContent.jsx
+++ components/card/BestSelling/MainDataContent.jsx
@@ -5,6 +5,7 @@ import { fetchServerSideData } from "@/helpers/ServerSideDataFetching";
 const MainBestSellingDataContent = async () => {
   const url = `${process.env.API_URL}/api/v1/best-selling-product`;
   const data = await fetchServerSideData(url);
+  // console.log(data.data[0].products);
   return (
     <main>
       <AllCards bestSellingProducts={data} />
