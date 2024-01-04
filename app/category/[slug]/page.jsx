01d55aec9edfe2b5757db45c2ef37c1115ed85a9
nuruzaman-milon <njmilon1@gmail.com>
2024-01-04 19:57:17 +0600

    update

--- app/category/[slug]/page.jsx
+++ app/category/[slug]/page.jsx
@@ -6,18 +6,18 @@ import React from "react";
 const SubCategoryPage = async ({ params }) => {
   const url = `${process.env.API_URL}/api/v1/sub-category/${params.slug}`;
   const data = await fetchServerSideData(url);
-  // console.log("slug", data.data[0]._id);
+  console.log("slug", data.data[0]._id);
 
   const filterUrl = `${process.env.API_URL}/api/v1/product/published?subCategory=${data?.data[0]?._id}`;
   const subCategoryData = await fetchServerSideData(filterUrl);
-  // console.log("subCategoryData", subCategoryData);
+  console.log("subCategoryData", subCategoryData);
   return (
     <main>
       <div>
         <DynamicSubCategoryContent
           data={subCategoryData}
           subCategoryData={data}
-        />{" "}
+        />
       </div>
     </main>
   );
--- helpers/ServerSideDataFetching.jsx
+++ helpers/ServerSideDataFetching.jsx
@@ -2,8 +2,8 @@ export const fetchServerSideData = async (url) => {
   const res = await fetch(url, {
     next: { revalidate: 300 },
   });
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
+  // if (!res.ok) {
+  //   throw new Error("Failed to fetch data");
+  // }
   return res.json();
 };
