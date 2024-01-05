ddabafd9e616aa6e533cc8d2d23fbf061ef13cc4
nuruzaman <njmilon1@gmail.com>
2024-01-05 11:37:02 +0600

    subcategory data fetching issue solve

--- app/category/[slug]/page.jsx
+++ app/category/[slug]/page.jsx
@@ -1,24 +1,22 @@
-import FestivalComponent from "@/components/Festivals/SingleFestivalContent";
+import React from "react";
 import DynamicSubCategoryContent from "@/components/SubCategory/DynamicSubCategory/DynamicSubCategoryContent";
 import { fetchServerSideData } from "@/helpers/ServerSideDataFetching";
-import React from "react";
 
 const SubCategoryPage = async ({ params }) => {
-  const url = `${process.env.API_URL}/api/v1/sub-category/${params.slug}`;
+  const url = `${process.env.API_URL}/api/v1/sub-category/slug/${params.slug}`;
+  // console.log("url", url);
   const data = await fetchServerSideData(url);
-  console.log("slug", data.data[0]._id);
+  // console.log("slug", data.data[0]._id);
 
   const filterUrl = `${process.env.API_URL}/api/v1/product/published?subCategory=${data?.data[0]?._id}`;
   const subCategoryData = await fetchServerSideData(filterUrl);
-  console.log("subCategoryData", subCategoryData);
+  // console.log("subCategoryData", subCategoryData);
   return (
     <main>
-      <div>
         <DynamicSubCategoryContent
           data={subCategoryData}
           subCategoryData={data}
         />
-      </div>
     </main>
   );
 };
