a5b96e16d834e24bef5c8468e421a9700bd8166e
nuruzaman-milon <njmilon1@gmail.com>
2024-06-25 15:24:59 +0600

    solve discount percentage issue

--- helpers/CalculateDiscountPercentage.js
+++ helpers/CalculateDiscountPercentage.js
@@ -4,8 +4,8 @@ export const CalculatePercentage = (regularPrice, salePrice) => {
   const regular = Number(regularPrice);
   const sale = Number(salePrice);
   if (regular > sale) {
-    const percentage = Math.ceil(((regular - sale) / regular) * 100);
-    return percentage;
+    const percentage = ((regular - sale) / regular) * 100;
+    return percentage.toFixed(0);
   } else {
     return 0;
   }
