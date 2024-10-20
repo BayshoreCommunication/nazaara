ed913e2e985aa7fad2bb71de7b78fbea8b59d947
Md.Nuruzaman Milon <32028951+nuruzaman-milon@users.noreply.github.com>
2024-10-20 15:02:44 +0600

    test messenger chat

--- components/MessengerChat.jsx
+++ components/MessengerChat.jsx
@@ -8,9 +8,9 @@ const MessengerChat = () => {
     <MessengerCustomerChat
       pageId="115797558072759" // Replace with your Facebook Page ID
       appId="1153289488711586" // Replace with your Facebook App ID
-      themeColor="#0084ff" // Optional: Customize the color
-      loggedInGreeting="Hi! How can we help you?"
-      loggedOutGreeting="Hi! Please log in to chat with us."
+      //   themeColor="#0084ff" // Optional: Customize the color
+      //   loggedInGreeting="Hi! How can we help you?"
+      //   loggedOutGreeting="Hi! Please log in to chat with us."
     />
   );
 };
